80180be79ad0574326b0b72086f85a2a0abe5d39
Shahab Mustafa <you@example.com>
2022-09-24 22:31:17 +0500

    make pizza app

---
+++ lib/view/splash_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
